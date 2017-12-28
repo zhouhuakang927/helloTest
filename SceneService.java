f965d5182b89d59c07acd12be02869242ad03ad4
zhouhuakang <zhou_huakang@hoperun.com>
2017-12-28 14:36:16 +0800

    提交文件

---
+++ SceneService.java
@@ -0,0 +1,40 @@
+package com.jettech.tmp.service;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jettech.tmp.dao.SceneDao;
+import com.jettech.tmp.model.TapScene;
+import com.jettech.tmp.model.User;
+/**
+ * 
+* @ClassName: SceneService
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author leiduo
+* @date 2017年3月31日 下午8:16:04
+*
+ */
+@Service
+@Transactional
+public class SceneService extends BaseService<TapScene> {
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(SceneService.class);
+	
+	@Autowired
+	private SceneDao sceneDao;
+	
+	@PostConstruct
+	private void postConstruct() {
+		baseDao = sceneDao;
+	}
+
+	public TapScene findByName(String name) {
+		return sceneDao.findBytapName(name);
+	}
+	
+}
